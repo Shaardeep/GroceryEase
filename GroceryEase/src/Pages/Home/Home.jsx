66ab0ae8846974d10ece1156201f391d13570b26
Somnath Mandal <somnathmandal6294@gmail.com>
2023-09-28 20:50:19 +0530

    Create BannerSlider

--- GroceryEase/src/Pages/Home/Home.jsx
+++ GroceryEase/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+import BannerSlider from "../../Components/Banners/BannerSlider";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <BannerSlider/>
       <Footer />
     </div>
   );
