ffdc71052d7a9cd51094a324b02dab37ea5c6206
Somnath Mandal <somnathmandal6294@gmail.com>
2023-09-30 21:45:39 +0530

    Created Products

--- GroceryEase/src/Pages/Home/Home.jsx
+++ GroceryEase/src/Pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import BannerSlider from "../../Components/Banners/BannerSlider";
+import HomeCategories from "../../Components/Category/HomeCategories";
+import Product_Sidebar from "../../Components/Product/Product_Sidebar";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <BannerSlider/>
+      <HomeCategories/>
+      <Product_Sidebar/>
       <Footer />
     </div>
   );
