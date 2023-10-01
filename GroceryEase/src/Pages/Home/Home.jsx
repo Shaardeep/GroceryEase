5028828ae871695fc23bc53a602e5e116079adbb
Somnath Mandal <somnathmandal6294@gmail.com>
2023-10-01 22:18:13 +0530

    Created Footer section

--- GroceryEase/src/Pages/Home/Home.jsx
+++ GroceryEase/src/Pages/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-import Footer from "../../Components/Footer/Footer";
 import BannerSlider from "../../Components/Banners/BannerSlider";
 import HomeCategories from "../../Components/Category/HomeCategories";
 import Product_Sidebar from "../../Components/Product/Product_Sidebar";
+import FooterFirst from "../../Components/Footer/FooterFirst";
+import FooterSecond from "../../Components/Footer/FooterSecond";
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <BannerSlider/>
-      <HomeCategories/>
-      <Product_Sidebar/>
-      <Footer />
+      <BannerSlider />
+      <HomeCategories />
+      <Product_Sidebar />
+      <FooterFirst />
+      <FooterSecond />
     </div>
   );
 }
