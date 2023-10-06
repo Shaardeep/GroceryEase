7780a21c73a09efb2cb434851d523368cbf8e393
Somnath Mandal <somnathmandal6294@gmail.com>
2023-10-06 20:34:15 +0530

    processing of creating productpage

--- GroceryEase/src/Pages/Product/ProductPage.jsx
+++ GroceryEase/src/Pages/Product/ProductPage.jsx
@@ -1,9 +1,209 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import img1 from "../../assets/Images/1.png";
+import img2 from "../../assets/Images/2.png";
+import img3 from "../../assets/Images/3.png";
+import Navbar from "../../Components/Navbar/Navbar";
+import FooterFirst from "../../Components/Footer/FooterFirst";
+import FooterSecond from "../../Components/Footer/FooterSecond";
+import './ProductPage.css';
 
 const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
-}
+  const { proid } = useParams();
+  const [imageset, setImageSet] = useState(null);
+  const [productdata, setProductData] = useState([]);
+  const [activeimg, setActiveImage] = useState({});
+  const [count, setCount] = useState(1);
 
-export default ProductPage
+  const getProductdatabyid = async () => {
+    let temp = {
+      code: 200,
+      Message: "Success",
+      Data: [
+        {
+          ProductId: 1,
+          ProductName: "Product 1",
+          ProductDescription: "Product 1 Description",
+          ProductImage: [
+            {
+              id: 1,
+              image: img1,
+            },
+            {
+              id: 2,
+              image: img2,
+            },
+            {
+              id: 3,
+              image: img3,
+            },
+          ],
+          ProductCode: "P1",
+          ProductCategory: "Category 1",
+          ProductSubCategory: "Sub Category 1",
+          ProductBrand: "Brand 1",
+          ProductColor: "Color 1",
+          ProductSize: "Size 1",
+          ProductWeight: "Weight 1",
+          ProductMaterial: "Material 1",
+          ProductQuantity: 10,
+          ProductUnit: "Unit 1",
+          ProductPrice: 100,
+          SalesPrice: 80,
+          ProductDiscount: 20,
+          ProductDiscountType: "Percentage",
+          ProductTax: 20,
+          ProductTaxType: "Percentage",
+          ProductShippingCharge: 20,
+          ProductShippingChargeType: "Percentage",
+          ProductShippingTime: "1-2 days",
+          ProductShippingTimeType: "Days",
+          ProductShippingLocation: "Location 1",
+          ProductShippingLocationType: "Country",
+          ProductShippingReturnPolicy: "Return Policy 1",
+          ProductShippingReturnPolicyType: "Days",
+          ProductShippingReturnPolicyDescription: "Return Policy Description 1",
+          ProductShippingReturnPolicyDescriptionType: "Days",
+          ProductReviews: [
+            {
+              ReviewId: 1,
+              Name: "Harshal Jain",
+              Email: "",
+              Rating: 5,
+              Date: "2021-08-01",
+              Review:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            },
+            {
+              ReviewId: 2,
+              Name: "Viraj",
+              Email: "",
+              Rating: 1,
+              Date: "2021-08-01",
+              Review:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            },
+            {
+              ReviewId: 3,
+              Name: "Harshal Jain",
+              Email: "",
+              Rating: 4,
+              Date: "2021-08-01",
+              Review:
+                "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            },
+          ],
+        },
+      ],
+    };
+    if (temp.code === 200) {
+      setImageSet(temp.Data[0].ProductImage);
+      setProductData(temp.Data[0]);
+      setActiveImage(temp.Data[0].ProductImage[0]);
+    }
+  };
+
+  useEffect(() => {
+    getProductdatabyid();
+  }, []);
+  return (
+    <div className="productpage">
+      <Navbar />
+      <div className="pc1">
+        <Link to="/">
+          <button className="goback">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+            Go Back
+          </button>
+        </Link>
+
+        <div className="c11">
+          <div className="imgset">
+            {imageset &&
+              imageset?.map((item, index) => {
+                return (
+                  <div
+                    className="imgsmall"
+                    onClick={() => {
+                      setActiveImage(item);
+                    }}
+                  >
+                    <img
+                      src={item.image}
+                      alt=""
+                      className={activeimg.id == item.id ? "active" : ""}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+          <div className="imgbig">
+            <img src={activeimg.image} alt="" />
+          </div>
+        </div>
+        <div className="c12">
+          <h1 className="head"> {productdata.ProductName} </h1>
+          <div className="c121">
+            <p className="price">
+              ${productdata.SalesPrice * count}
+              <span>${productdata.ProductPrice * count}</span>
+            </p>
+            <div className="incrdecr">
+              <button
+                onClick={() => {
+                  if (count > 1) {
+                    setCount(count - 1);
+                  }
+                }}
+              >
+                -
+              </button>
+              <p>{count}</p>
+              <button
+                onClick={() => {
+                  if (count < 10) {
+                    setCount(count + 1);
+                  }
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="btncont">
+            <button
+              onClick={() => {
+                addtocart();
+              }}
+            >
+              Add to Cart
+            </button>
+            <button
+              onClick={() => {
+                alert("Buy Now");
+              }}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <FooterFirst/> */}
+    </div>
+  );
+};
+
+export default ProductPage;
